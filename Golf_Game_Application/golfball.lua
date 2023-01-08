4f91dbc4af8d385b5ef96894af66100f3ae31189
MorranMogge <77781581+MorranMogge@users.noreply.github.com>
2023-01-08 20:52:02 +0100

    Velocity decrease is now more correct

--- Golf_Game_Application/golfball.lua
+++ Golf_Game_Application/golfball.lua
@@ -10,34 +10,22 @@ function golfball:OnUpdate(delta)
 	local vel = scene.GetComponent(self.ID, "velocity")
 	pos.x = pos.x + delta * vel.velocityX
 	pos.y = pos.y + delta * vel.velocityY
-	
-	if (vel.velocityX < 0) then
-		vel.velocityX = math.min(vel.velocityX + delta * constant, 0)
-	else
-		vel.velocityX = math.max(vel.velocityX - delta * constant, 0)
-	end
-	
-	if (vel.velocityY < 0) then
-		vel.velocityY = math.min(vel.velocityY + delta * constant, 0)
-	else
-		vel.velocityY = math.max(vel.velocityY - delta * constant, 0)
-	end
-
-	scene.SetComponent(self.ID, "position", pos)
-	if (vel.velocityX == 0 and vel.velocityY == 0) 
-	then
-		print("Removed Velocity")
-		scene.RemoveComponent(self.ID, "velocity")
-	else	
+	speed = math.sqrt(vel.velocityX*vel.velocityX + vel.velocityY*vel.velocityY)
+	newSpeed = speed - delta * constant
+	ratio = newSpeed/speed
+	if (speed > 5) then
+		vel.velocityX = vel.velocityX * ratio
+		vel.velocityY = vel.velocityY * ratio
 		scene.SetComponent(self.ID, "velocity", vel)
+	else
+		scene.RemoveComponent(self.ID, "velocity")
 	end
+	scene.SetComponent(self.ID, "position", pos)
 end
 
 function golfball:collide(x, y)
-	print(self.ID)
 	local pos = scene.GetComponent(self.ID, "position")
 	local vel = scene.GetComponent(self.ID, "velocity")
-	-- local collisionInfo = scene.GetComponent(self.ID, "collision")	
 	if (x) then
 		pos.x = pos.x - vel.velocityX * 4/144 
 		vel.velocityX = -vel.velocityX * 0.9
