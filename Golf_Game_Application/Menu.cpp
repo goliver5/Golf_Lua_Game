59663ef0f4c855002c64852351ee1f1d7cfdc1f7
MorranMogge <77781581+MorranMogge@users.noreply.github.com>
2023-01-08 12:52:23 +0100

    Added Credits text

--- Golf_Game_Application/Menu.cpp
+++ Golf_Game_Application/Menu.cpp
@@ -37,5 +37,6 @@ void Menu::render()
 	DrawText("Play game", 800 / 2 - 50, 450 / 4 - 50, 20, DARKGRAY);
 	DrawText("Exit Game", 800 / 2 - 240, 480 / 2 + 50, 20, DARKGRAY);
 	DrawText("Credits", 800 / 2 + 150, 480 / 2 + 50, 20, DARKGRAY);
+	DrawText("Editor", 800 / 2 - 130, 480 / 2 - 75, 20, DARKGRAY);
 	EndDrawing();
 }
